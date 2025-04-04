e802c13ee3039c47e2aff1d1341732f7cc473a4b
Aleksey Konyakhin <konyaxin.1998@gmail.com>
2025-04-04 13:27:45 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <GameHeader />
+        <div class="game-container">
+            <GameFigure />
+
+            <GameWrongLetters />
+
+            <GameWord />
+        </div>
+
+        <!-- Container for final message -->
+        <GamePopup />
+
+        <!-- Notification -->
+        <GameNotification />
+    </div>
+</template>
+
+<script setup lang="ts">
+import GameFigure from './components/GameFigure.vue'
+import GameHeader from './components/GameHeader.vue'
+import GameNotification from './components/GameNotification.vue'
+import GamePopup from './components/GamePopup.vue'
+import GameWord from './components/GameWord.vue'
+import GameWrongLetters from './components/GameWrongLetters.vue'
+</script>
+
+<style scoped></style>
